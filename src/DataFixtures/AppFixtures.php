5fad17847685737579fcb154ee4e8e8e928c4280
berti <berti@epse.be>
2022-10-07 14:36:24 +0200

    cours 2 - HasLifecycleCallbacks entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,7 +4,6 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
-use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -15,18 +14,16 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
         $faker = Factory::create('fr_FR');
-        $slugify = new Slugify();
+
 
         for($i = 1; $i <= 30; $i++)
         {
             $ad = new Ad();
             $title = $faker->sentence();
-            $slug = $slugify->slugify($title);
             $introduction = $faker->paragraph(2);
             $content = '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>';
 
             $ad->setTitle($title)
-                ->setSlug($slug)
                 ->setCoverImage('https://picsum.photos/1000/350')
                 ->setIntroduction($introduction)
                 ->setContent($content)
