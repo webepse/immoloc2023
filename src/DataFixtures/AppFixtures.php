d5d2c7a5598d1744253df1194f285af7634a518c
berti <berti@epse.be>
2022-10-07 13:55:28 +0200

    cours 2 - fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ad;
+use Faker\Factory;
+use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +14,29 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $slugify = new Slugify();
+
+        for($i = 1; $i <= 30; $i++)
+        {
+            $ad = new Ad();
+            $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>';
+
+            $ad->setTitle($title)
+                ->setSlug($slug)
+                ->setCoverImage('https://picsum.photos/1000/350')
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(rand(40,200))
+                ->setRooms(rand(1,5));
+            
+            $manager->persist($ad);
+
+        }
+
 
         $manager->flush();
     }
