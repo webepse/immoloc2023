5800cc63531c0074ec041f8eac65ce49ecf9a010
berti <berti@epse.be>
2022-10-07 16:03:24 +0200

    cours 3 - param converter

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use App\Repository\AdRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +19,26 @@ class AdController extends AbstractController
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads
+        ]);
+    }
+
+    // #[Route('/ads/{slug}', name:'ads_show')]
+    // public function show(string $slug, ManagerRegistry $doctrine):Response
+    // {
+    //     $repo = $doctrine->getRepository(Ad::class);
+    //     $ad = $repo->findOneBySlug($slug);
+
+    //     return $this->render('ad/show.html.twig',[
+    //         'ad' => $ad
+    //     ]);
+    // }
+
+
+    #[Route('/ads/{slug}', name:'ads_show')]
+    public function show(string $slug, Ad $ad):Response
+    {
+        return $this->render('ad/show.html.twig',[
+            'ad' => $ad
         ]);
     }
 }
