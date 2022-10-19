745aaf70f60c9c151deaf7f58fac9b859f3ce019
Boderio <boderiotrigan@hotmail.com>
2022-10-19 10:12:03 +0200

    cours 6 - inscription

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AccountController extends AbstractController
 {
@@ -38,4 +43,42 @@ class AccountController extends AbstractController
         // ..
     }
 
+    /**
+     * Permet d'afficher le formulaire d'inscription d'un utilisateur et de l'ajouter à la base de données
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
+    #[Route("/register", name:"account_register")]
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $hash = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé"
+            );
+
+            return $this->redirectToRoute('account_login');
+
+        }
+
+        return $this->render("account/registration.html.twig",[
+            'myform' => $form->createView()
+        ]);
+
+
+    }
 }
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -3,24 +3,28 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\AbstractType;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class RegistrationType extends AbstractType
+class RegistrationType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('picture')
-            ->add('introduction')
-            ->add('description')
-            ->add('slug')
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom","Votre prénom..."))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom de famille..."))
+            ->add('email', EmailType::class, $this->getConfiguration("Email", "Votre adresse e-mail..."))
+            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "URL de votre avatar..."))
+            ->add('password', PasswordType::class, $this->getConfiguration("Mot de passe", "Votre mot de passe"))
+            ->add('passwordConfirm', PasswordType::class, $this->getConfiguration("Confirmation du mot de passe", "Veuillez confirmer votre mot de passe"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Présentation rapide"))
+            ->add('description', TextareaType::class, $this->getConfiguration("Description détaillée", "Présentez vous avec un peu plus de détails"))
         ;
     }
 
