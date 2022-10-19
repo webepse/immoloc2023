643bc112678eedebfbda3e02bb140e00f260e6b7
Boderio <boderiotrigan@hotmail.com>
2022-10-19 09:34:01 +0200

    cours 6 - throttling + ApplicationType

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use App\Form\ImageType;
-use Symfony\Component\Form\AbstractType;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -14,27 +14,8 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
-class AnnonceType extends AbstractType
+class AnnonceType extends ApplicationType
 {
-
-    /**
-     * Permet d'avoir la configuration de base d'un champ
-     *
-     * @param string $label
-     * @param string $placeholder
-     * @param array $options
-     * @return array
-     */
-    private function getConfiguration(string $label, string $placeholder, array $options=[]): array
-    {
-        return array_merge_recursive([
-            'label' => $label,
-            'attr' => [
-                'placeholder' => $placeholder
-            ]
-        ], $options);
-    }
-
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
