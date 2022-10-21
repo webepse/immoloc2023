93cf201cfec4c45982e25ec447d6a28a960b4fc6
berti <berti@epse.be>
2022-10-21 14:02:40 +0200

    cours 8 - upload image v1

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -64,6 +65,25 @@ class AccountController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid())
         {
+            // gestion de mon image
+            $file = $form['picture']->getData();
+            if(!empty($file))
+            {
+                $originalFilename = pathinfo($file->getClientOriginalName(),PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin;Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename."-".uniqid().".".$file->guessExtension();
+                try{
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                }catch(FileException $e)
+                {
+                    return $e->getMessage();
+                }
+                $user->setPicture($newFilename);
+            }
+
             $hash = $hasher->hashPassword($user, $user->getPassword());
             $user->setPassword($hash);
 
--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -6,7 +6,6 @@ use App\Entity\User;
 use App\Form\ApplicationType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -19,7 +18,6 @@ class AccountType extends ApplicationType
             ->add('firstName', TextType::class, $this->getConfiguration("Prénom","Votre prénom..."))
             ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom de famille..."))
             ->add('email', EmailType::class, $this->getConfiguration("Email", "Votre adresse e-mail..."))
-            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "URL de votre avatar..."))
             ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Présentation rapide"))
             ->add('description', TextareaType::class, $this->getConfiguration("Description détaillée", "Présentez vous avec un peu plus de détails"))
         ;
