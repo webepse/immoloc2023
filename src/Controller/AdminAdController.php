f8cb61eb591455d81e0eeda8a1a26f263d2d615f
Boderio <boderiotrigan@hotmail.com>
2022-11-28 11:06:37 +0100

    cours 14 - admin des annonces v2

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,18 +2,90 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminAdController extends AbstractController
 {
+    /**
+     * Permet d'afficher les annonces pour l'administration
+     *
+     * @param AdRepository $repo
+     * @return Response
+     */
     #[Route('/admin/ads', name: 'admin_ads_index')]
     public function index(AdRepository $repo): Response
     {
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll(),
         ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     * @param Ad $ad
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/admin/ads/{id}/edit", name: "admin_ads_edit")]
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(AnnonceType::class, $ad);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été modifiée"
+            );
+        }
+
+        return $this->render("admin/ad/edit.html.twig",[
+            "ad" => $ad,
+            "myForm" => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * Permet de supprimer une annonce
+     *
+     * @param Ad $as
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/admin/ads/{id}/delete", name: "admin_ads_delete")]
+    public function delete(Ad $ad, EntityManagerInterface $manager): Response
+    {
+        // one ne peut pas supprimer une annonce qui possède des réservations
+        if(count($ad->getBookings()) > 0)
+        {
+            // afficher qu'on ne peut pas supprimer 
+            $this->addFlash(
+                'warning',
+                "Vous ne pouvez pas supprier l'annonce <strong>id: {$ad->getId()} - {$ad->getTitle()}</strong> car elle possède des réservations"
+            );
+        }else{
+            // supprimer + flash
+            $this->addFlash(
+                "success",
+                "L'annonce <strong>id: {$ad->getId()} - {$ad->getTitle()}</strong> a bien été supprimée"
+            );
+            $manager->remove($ad);
+            $manager->flush();
+        }
+
+        return $this->redirectToRoute('admin_ads_index');
     }
 }
